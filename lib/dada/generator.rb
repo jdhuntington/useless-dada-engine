9173086578980455c53675c0d69cb4b3be21daf2
JD Huntington <jdhuntington@gmail.com>
2012-08-30 08:54:47 -0700

    Generator with trivial example. Fixed `bin/dad`.

--- lib/dada/generator.rb
+++ lib/dada/generator.rb
@@ -5,23 +5,13 @@ module Dada
     end
 
     def generate
-      strings = generate_helper(:main).flatten
-      strings.each do |s|
-        abort "#{s.inspect} is not a string" unless s.is_a?(String)
-      end
-      strings.join('')
+      starting_phrase = term
+      starting_phrase.resolve(self)
+      starting_phrase.to_s
     end
 
-    def generate_helper(definition)
-      selection = @atoms[definition].random
-      selection = selection.is_a?(Array) ? selection : [selection]
-      selection.map do |x|
-        if x.is_a? Symbol
-          generate_helper x
-        else
-          x
-        end
-      end
+    def term(name=:main)
+      @atoms[name].random.dup
     end
   end
 end
