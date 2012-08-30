244ce655a6100b0cf55d6b32278a4c3937140cd7
JD Huntington <jdhuntington@gmail.com>
2012-08-29 21:45:18 -0700

    Follow rules

--- lib/dada/generator.rb
+++ lib/dada/generator.rb
@@ -5,9 +5,23 @@ module Dada
     end
 
     def generate
-      bits = []
-      bits << @atoms[:main].random
-      bits.join('')
+      strings = generate_helper(:main).flatten
+      strings.each do |s|
+        abort "#{s.inspect} is not a string" unless s.is_a?(String)
+      end
+      strings.join('')
+    end
+
+    def generate_helper(definition)
+      selection = @atoms[definition].random
+      selection = selection.is_a?(Array) ? selection : [selection]
+      selection.map do |x|
+        if x.is_a? Symbol
+          generate_helper x
+        else
+          x
+        end
+      end
     end
   end
 end
--- spec/dada/generator_spec.rb
+++ spec/dada/generator_spec.rb
@@ -6,4 +6,11 @@ describe Dada::Generator do
     g = Dada::Generator.new(Dada::Parser.new(contents))
     g.generate.should == "Hello, world!"
   end
+
+  it 'should generate a string from 5 rules' do
+    contents = fixture('5-rules')
+    g = Dada::Generator.new(Dada::Parser.new(contents))
+    expects_either = %w{ foo bar }
+    expects_either.should include(g.generate)
+  end
 end
